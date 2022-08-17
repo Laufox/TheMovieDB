76aa20ac08cd31f1dc41c9f0094841c386860f3f
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-08-17 17:19:11 +0200

    Setting up access to API service

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
 import Container from 'react-bootstrap/Container'
+import { useQuery } from 'react-query'
+import TheMovieDBAPI from '../services/TheMovieDBAPI'
 
 const HomePage = () => {
+
+	const { data, error } = useQuery('genres', TheMovieDBAPI.getGenres)
+
 	return (
 		<Container className="py-3">
 			<h1>Welcome!</h1>
+			{
+				error && (
+					<p>There was an error</p>
+				)
+			}
+			{
+				data && data.genres.map( genre => <p key={genre.id}>{genre.name}</p> )
+			}
 		</Container>
 	)
 }
