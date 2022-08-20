1220a27cf76e88cb609fdcb8c1513dc8aeeea97b
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-08-20 19:15:54 +0200

    Adds more info and styling about each movie in movie list

--- src/pages/GenreList.jsx
+++ src/pages/GenreList.jsx
@@ -2,6 +2,7 @@ import { useParams, useSearchParams } from "react-router-dom"
 import useDiscoverByGenre from "../hooks/useDiscoverByGenre"
 import Card from 'react-bootstrap/Card'
 import Container from "react-bootstrap/Container"
+import Button from 'react-bootstrap/Button'
 import { useState, useEffect } from "react"
 import Pagination from "../components/Pagination"
 
@@ -43,12 +44,25 @@ const GenreList = () => {
                 movies && (
                     
                     <Container>
-                        
+
                         <div className="movie-container">
                             {
                                 movies.results.map( movie => (
                                     <Card key={movie.id} className="movie-container-item">
-                                        <Card.Body>{movie.title}</Card.Body>
+                                        <Card.Body className="card-body">
+                                            
+                                            <Card.Title className="movie-heading">{movie.title}</Card.Title>
+                                            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                                            <Card.Text className="additional-info">
+                                                <span>{movie.release_date.slice(0, movie.release_date.indexOf('-'))}</span>
+                                                <span>|</span>
+                                                <span>{movie.original_language.toUpperCase()}</span>
+                                                <span>|</span>
+                                                <span>{movie.vote_average}</span>
+                                            </Card.Text>
+                                            <Button>Go to</Button>
+
+                                        </Card.Body>
                                     </Card>
                                 ) )
                             }
