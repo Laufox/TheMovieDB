83b8e046bc3a5603a9884cb61370a91e2bbe6a5b
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-08-26 16:04:48 +0200

    Makes it possible to open/close the filterbox

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -6,6 +6,7 @@ import Button from 'react-bootstrap/Button'
 
 const FilterBox = ( { genres } ) => {
 
+    const [isBoxOpen, setIsBoxOpen] = useState(true)
     // State for search query typed in search form
     const [searchInput, setSearchInput] = useState('')
     // Call useNavigate for possibillity to redirect to other pages
@@ -22,41 +23,49 @@ const FilterBox = ( { genres } ) => {
     
     return (
         <div className='filterbox'>
-        
-            <ListGroup className='genre-list'>
-                {
-                    // Render existing genres
-                    genres.map(genre => (
-                        <ListGroup.Item key={genre.id}>
-                            <Link to={`/genre/${genre.id}/${genre.name.replace(/\s/g, '-').toLowerCase()}?page=1`}>{genre.name}</Link>
-                        </ListGroup.Item>
-                    ))
-                }
-            </ListGroup>
 
-            <ListGroup className='popular-list'>
+            <div>
+                <ListGroup className={ isBoxOpen ? 'genre-list' : 'hidden'}>
+                    {
+                        // Render existing genres
+                        genres.map(genre => (
+                            <ListGroup.Item key={genre.id}>
+                                <Link to={`/genre/${genre.id}/${genre.name.replace(/\s/g, '-').toLowerCase()}?page=1`}>{genre.name}</Link>
+                            </ListGroup.Item>
+                        ))
+                    }
 
-                <ListGroup.Item>
-                    <Link to={`/new?page=1`}>Latest</Link>
-                </ListGroup.Item>
+                </ListGroup>
+                <div className='toggle-genre-list' onClick={() => {setIsBoxOpen( prevState => !prevState )}}>
+                    { isBoxOpen ? '⬆️' : '⬇️' }
+                </div>
+            </div>
 
-                <ListGroup.Item>
-                    <Link to={`/popular?page=1`}>Popular</Link>
-                </ListGroup.Item>
+            <div>
+                <ListGroup className={ isBoxOpen ? 'popular-list' : 'hidden'}>
 
-                <ListGroup.Item>
-                    <Link to={`/toprated?page=1`}>Top Rated</Link>
-                </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Link to={`/new?page=1`}>Latest</Link>
+                    </ListGroup.Item>
 
-                <ListGroup.Item>
-                    <Link to={`/trending/day?page=1`}>Trending 24h</Link>
-                </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Link to={`/popular?page=1`}>Popular</Link>
+                    </ListGroup.Item>
 
-                <ListGroup.Item>
-                    <Link to={`/trending/week?page=1`}>Trending 7d</Link>
-                </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Link to={`/toprated?page=1`}>Top Rated</Link>
+                    </ListGroup.Item>
 
-            </ListGroup>
+                    <ListGroup.Item>
+                        <Link to={`/trending/day?page=1`}>Trending 24h</Link>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                        <Link to={`/trending/week?page=1`}>Trending 7d</Link>
+                    </ListGroup.Item>
+
+                </ListGroup>
+            </div>
 
             <Form onSubmit={handleSubmit} className='movie-search'>
                 <Form.Control 
