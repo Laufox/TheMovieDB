8ee9a66567f38544a337fea51087f506def18152
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-08-29 10:36:19 +0200

    Alters sidebar design

--- src/components/PrevSeenMovies.jsx
+++ src/components/PrevSeenMovies.jsx
@@ -2,14 +2,11 @@ import useGetLatestViewedMovies from "../hooks/useGetLatestViewedMovies"
 import LoadingSpinner from "./LoadingSpinner"
 import AlertBox from "./AlertBox"
 import MovieCardTiny from "./MovieCardTiny"
-import { useState } from "react"
 
 const PrevSeenMovies = () => {
 
     // Get query data from useGetLatestViewedMovies hook
     const {data: pastMovies, error, isError, isLoading} = useGetLatestViewedMovies()
-
-    const [isPastOpen, setIsPastOpen] = useState()
 
     return (
 
@@ -27,12 +24,11 @@ const PrevSeenMovies = () => {
 
                 <div className="pastmovies">
 
-                    <span onClick={ () => { setIsPastOpen( prevState => !prevState ) } } >{ isPastOpen ? '➡️' : '⬅️' }</span>
                     <h3>Past views:</h3>
 
                     {
                         // Loop through movies to render each of them 
-                        isPastOpen && pastMovies.map(movie => (
+                        pastMovies.map(movie => (
                             <MovieCardTiny movie={movie} key={movie.id} />
                         ))
                     }
